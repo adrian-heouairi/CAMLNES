248e593294c5327218e01a3aef00ab3a09c2b2f6
Adrian HEOUAIRI <adrian.heouairi@etu.u-paris.fr>
2023-02-05 10:43:02 +0100

    Improve inst_test_v5 suite tests

--- CAMLNES/test/instr_test_v5.ml
+++ CAMLNES/test/instr_test_v5.ml
@@ -9,8 +9,8 @@ let tests = [| "01-basics"; "02-implied"; "03-immediate"; "04-zero_page"; "05-zp
 let rec get_message sofar =
   let char = char_of_int @@ Bus.read (0x6004 + List.length sofar) in
   let char = if char = '\n' then ' ' else char in
-  print_char char;
-  if char = '\000' then []
+  (*print_char char;*)
+  if char = '\000' then sofar
   else get_message (sofar @ [char]);;
 
 exception Break_loop;;
@@ -18,12 +18,16 @@ exception Break_loop;;
 for test_number = 0 to Array.length tests - 1 do
   print_string (tests.(test_number) ^ ": ");
 
+  (* Generate a log for a specific test *)
+  (*Cpu.disable_logging ();
+  (if tests.(test_number) = "15-brk" then Cpu.enable_logging @@ tests.(test_number) ^ "-cpu.log");*)
+
   Init.init ("../../../test/instr_test_v5/" ^ tests.(test_number) ^ ".nes");
 
   Bus.write 0x6000 0x80;
 
   ( try
-    for i = 1 to 1000000 do
+    for i = 1 to 10000000 do
       (*printf "%d:%02X " i @@ Bus.read 0x6001;*)
       let _ = i in
       Cpu.run_next_instruction ();
@@ -31,7 +35,8 @@ for test_number = 0 to Array.length tests - 1 do
     done
   with
     Break_loop ->
-      Printf.printf "Test ended with result code: 0x%02X; " (Bus.read 0x6000);
+      Printf.printf "Test ended with result code: 0x%02X (%s); " (Bus.read 0x6000)
+      (if Bus.read 0x6000 = 0 then "Success" else "Failure");
     | exc ->
       print_string ("Exception occurred during test: " ^ Printexc.to_string exc ^ "; ");
   );
@@ -41,10 +46,8 @@ for test_number = 0 to Array.length tests - 1 do
   (*print_endline @@ Int.to_string @@ Bus.read 0x6008*)
 
   print_string "Message: ";
-  let _ = get_message [] in
+  List.iter (Printf.printf "%c") (get_message []);
+  (* Alternative: print_endline @@ String.of_seq @@ List.to_seq @@ get_message [];; *)
   print_newline ();
-
-  (* TODO: Doesn't work *)
-  (*List.iter (Printf.printf "%c") (get_message []);*)
-  (*print_endline @@ String.of_seq @@ List.to_seq @@ get_message [];;*)
+  
 done
