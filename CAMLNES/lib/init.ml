9a63b1c13aa1d442517e06d9c782cf74a4aa435f
Adrian HEOUAIRI <adrian.heouairi@etu.u-paris.fr>
2023-02-05 10:42:15 +0100

    Reset CPU state when inserting a new cartridge

--- CAMLNES/lib/init.ml
+++ CAMLNES/lib/init.ml
@@ -1,5 +1,6 @@
 let init game_path =
   for i = 0 to 65535 do Bus.write i 0 done;
+  Cpu.reset_state ();
   Cartridge.parse_nes_file game_path;
   Cartridge.insert_PRG ();
   Cpu.state.program_counter <- Bus.read 0xFFFC + Bus.read 0xFFFD * 256;;
