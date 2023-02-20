9de006e8eaf2b8219322ceef3f94bb1cfc230992
Adrian HEOUAIRI <adrian.heouairi@etu.u-paris.fr>
2023-02-20 19:38:03 +0100

    Ajout de la préparation du PPU (ébauche)

--- CAMLNES/lib/init.ml
+++ CAMLNES/lib/init.ml
@@ -1,8 +1,9 @@
 let init game_path =
-  for i = 0 to 65535 do Bus.write i 0 done;
+  for i = 0 to 65535 do Bus.write i 0 done; (* Also disables NMI *)
   Cpu.reset_state ();
   Cartridge.parse_nes_file game_path;
   Cartridge.insert_PRG ();
-  Cpu.state.program_counter <- Bus.read 0xFFFC + Bus.read 0xFFFD * 256;;
+  Cpu.state.program_counter <- Bus.read 0xFFFC + Bus.read 0xFFFD * 256;
 
   (* Prepare PPU here *)
+  Cartridge.insert_CHR ();
