e906dc9b4a9d3cd18ce063a1d476e4f63cfadbc3
Adrian HEOUAIRI <adrian.heouairi@etu.u-paris.fr>
2023-02-20 19:30:37 +0100

    Commentaires indiquant comment faire la boucle principale

--- CAMLNES/bin/main.ml
+++ CAMLNES/bin/main.ml
@@ -1,5 +1,14 @@
 open CAMLNES;;
+(*open Tsdl;;*)
 
 Init.init Sys.argv.(1);;
 
-(* Here loop that executes CPU and PPU *)
+(* Here loop that executes CPU and PPU: *)
+
+(*début_de_frame = Unix.timestamp
+set cpu.nmi if nmis are activated
+exécuter le cpu pour approximativement une frame (en général il se bloquera dans une boucle)
+render image
+sleep autant qu'il faut jusqu'à la fin de la durée d'une frame selon $début_de_frame
+recommencer
+obtenir plusieurs longueurs comme durée d'un cycle, d'une frame etc*)
