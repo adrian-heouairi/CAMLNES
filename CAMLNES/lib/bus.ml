30768e6db6afeee6c844689bfe4acc9c58d23e46
Adrian HEOUAIRI <adrian.heouairi@etu.u-paris.fr>
2023-02-23 22:06:26 +0100

    Add support for OAMDMA

--- CAMLNES/lib/bus.ml
+++ CAMLNES/lib/bus.ml
@@ -1,10 +1,29 @@
+open Ppu_constants;;
+
 let bus = Array.make 65536 0;;
 
-let read addr = bus.(addr);;
+let read addr =
+  (*(if addr = oAMDATA then failwith "OAMDATA is not supported"
+  );*)
+  
+  bus.(addr);;
+
+let oamdma msb =
+  let bus_start_addr = msb lsl 8 in
+  let offset = read oAMADDR in
+  for i = 0 to 255 do
+    Ppumem.oam_write ((i + offset) mod 256) (read (bus_start_addr + i));
+  done;;
 
 let write addr byte =
   assert (0 <= byte && byte <= 255);
-  bus.(addr) <- byte;;
+  bus.(addr) <- byte;
+
+  (
+    (*if addr = oAMDATA then failwith "OAMDATA is not supported"*)
+    if addr = oAMDMA then oamdma byte
+  );
+;;
 
 let load_PRG_bank addr _PRG_bank = (* Bank is necessarily of size 16384 *)
   for i = addr to addr + 16383 do
