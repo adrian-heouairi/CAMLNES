03203375b85b94f475230c5fef29785e903008ac
Adrian HEOUAIRI <adrian.heouairi@etu.u-paris.fr>
2023-02-23 22:09:51 +0100

    Move PPU constants in a separate file

---
+++ CAMLNES/lib/ppu_constants.ml
@@ -0,0 +1,10 @@
+(* These addresses refer to CPU memory space *)
+let pPUCTRL = 0x2000;;
+let pPUMASK = 0x2001;;
+let pPUSTATUS = 0x2002;;
+let oAMADDR = 0x2003;;
+let oAMDATA = 0x2004;;
+let pPUSCROLL = 0x2005;;
+let pPUADDR = 0x2006;;
+let pPUDATA = 0x2007;;
+let oAMDMA = 0x4014;;
