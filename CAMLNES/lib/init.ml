2acd497dd7005e484d1c1c0748da4e1fad76c258
Adrian HEOUAIRI <adrian.heouairi@etu.u-paris.fr>
2023-02-23 22:10:33 +0100

    Clear bus by directly writing to the array

--- CAMLNES/lib/init.ml
+++ CAMLNES/lib/init.ml
@@ -1,5 +1,5 @@
 let init game_path =
-  for i = 0 to 65535 do Bus.write i 0 done; (* Also disables NMI *)
+  for i = 0 to 65535 do Bus.bus.(i) <- 0 done; (* Also disables NMI *)
   Cpu.reset_state ();
   Cartridge.parse_nes_file game_path;
   Cartridge.insert_PRG ();
