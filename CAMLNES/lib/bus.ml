63fd1653addf60d7e129a74aed9ae61347f3394c
Adrian HEOUAIRI <adrian.heouairi@etu.u-paris.fr>
2023-03-14 19:20:44 +0100

    Add checks to memory functions and format code

--- CAMLNES/lib/bus.ml
+++ CAMLNES/lib/bus.ml
@@ -4,9 +4,12 @@ let bus = Array.make 65536 0
 let vram_addr = ref (-1)
 let scroll_pos = ref (-1)
 
-let read_raw addr = bus.(addr)
+let read_raw addr =
+  assert (0 <= addr && addr <= 65535);
+  bus.(addr)
 
 let write_raw addr byte =
+  assert (0 <= addr && addr <= 65535);
   assert (0 <= byte && byte <= 255);
   bus.(addr) <- byte
 
@@ -14,17 +17,18 @@ let get_vram_addr_increment () =
   if read_raw _PPUCTRL land 0b100 = 0 then 1 else 32
 
 let read addr =
+  assert (0 <= addr && addr <= 65535);
   let ret = ref (-1) in
   let real_addr = ref addr in
-  if 0x2000 <= addr && addr <= 0x3FFF then
-    real_addr := addr mod 8 + 0x2000;
+  if 0x2000 <= addr && addr <= 0x3FFF then real_addr := (addr mod 8) + 0x2000;
 
   (* TODO Implement OAMDATA reads *)
-  if !real_addr = _PPUSTATUS then (vram_addr := -1; scroll_pos := -1);
+  if !real_addr = _PPUSTATUS then (
+    vram_addr := -1;
+    scroll_pos := -1);
   if !real_addr = _PPUDATA then (
     ret := Ppumem.read !vram_addr;
-    vram_addr := (!vram_addr + (get_vram_addr_increment ())) mod 0x4000
-  );
+    vram_addr := (!vram_addr + get_vram_addr_increment ()) mod 0x4000);
 
   if !ret <> -1 then !ret else bus.(!real_addr)
 
@@ -36,23 +40,22 @@ let do_OAMDMA msb =
   done
 
 let write addr byte =
+  assert (0 <= addr && addr <= 65535);
   assert (0 <= byte && byte <= 255);
 
   let real_addr = ref addr in
-  if 0x2000 <= addr && addr <= 0x3FFF then
-    real_addr := addr mod 8 + 0x2000;
+  if 0x2000 <= addr && addr <= 0x3FFF then real_addr := (addr mod 8) + 0x2000;
 
   (* TODO Implement OAMDATA writes *)
   if !real_addr = _PPUSCROLL then
-    (if !scroll_pos = -1 then scroll_pos := byte lsl 8
-    else scroll_pos := !scroll_pos + byte);
+    if !scroll_pos = -1 then scroll_pos := byte lsl 8
+    else scroll_pos := !scroll_pos + byte;
   if !real_addr = _PPUADDR then
-    (if !vram_addr = -1 then vram_addr := byte lsl 8
-    else vram_addr := !vram_addr + byte);
+    if !vram_addr = -1 then vram_addr := byte lsl 8
+    else vram_addr := !vram_addr + byte;
   if !real_addr = _PPUDATA then (
     Ppumem.write !vram_addr byte;
-    vram_addr := (!vram_addr + (get_vram_addr_increment ())) mod 0x4000
-  );
+    vram_addr := (!vram_addr + get_vram_addr_increment ()) mod 0x4000);
   if !real_addr = _OAMDMA then do_OAMDMA byte;
 
   bus.(!real_addr) <- byte
--- CAMLNES/lib/ppumem.ml
+++ CAMLNES/lib/ppumem.ml
@@ -1,17 +1,23 @@
 let memory = Array.make 0x4000 0
 
 (* TODO Implement mirroring for reads and writes to addresses >= 0x4000 *)
-let read addr = memory.(addr)
+let read addr =
+  assert (0 <= addr && addr < 0x4000);
+  memory.(addr)
 
 let write addr byte =
+  assert (0 <= addr && addr < 0x4000);
   assert (0 <= byte && byte <= 255);
   memory.(addr) <- byte
 
 let _OAM = Array.make 256 0
 
 (* Sprite memory *)
-let _OAM_read addr = _OAM.(addr)
+let _OAM_read addr =
+  assert (0 <= addr && addr <= 255);
+  _OAM.(addr)
 
 let _OAM_write addr byte =
+  assert (0 <= addr && addr <= 255);
   assert (0 <= byte && byte <= 255);
   _OAM.(addr) <- byte
