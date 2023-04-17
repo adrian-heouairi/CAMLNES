3ba05826a9f338049c7f0ba72cf31f1ba7fa826c
Adrian HEOUAIRI <adrian.heouairi@etu.u-paris.fr>
2023-04-17 19:42:31 +0200

    We have a start of rendering

--- CAMLNES/bin/main.ml
+++ CAMLNES/bin/main.ml
@@ -1,8 +1,21 @@
+(* main.ml is inspired from https://github.com/DuoSRX/ocamnes/blob/master/bin/main.ml *)
+
 open CAMLNES;;
 
-(*open Tsdl;;*)
+open Tsdl;;
 
-Cpu.enable_logging "cpu-main.log";;
+let sdl_wrapper arg = match arg with
+  | Ok x -> x
+  | Error (`Msg msg) -> print_endline ("SDL error: " ^ msg); exit 1;;
+
+sdl_wrapper @@ Sdl.init Sdl.Init.video;;
+let window = sdl_wrapper @@ Sdl.create_window ~w:256 ~h:240 "CAMLNES" Sdl.Window.(opengl + shown);;
+let renderer = sdl_wrapper @@
+  Sdl.create_renderer window ~flags:Sdl.Renderer.accelerated;;
+let texture = sdl_wrapper @@
+  Sdl.create_texture renderer Sdl.Pixel.format_rgb24 Sdl.Texture.access_streaming ~w:256 ~h:240;;
+
+(*Cpu.enable_logging "cpu-main.log";;*)
 Init.init Sys.argv.(1);
 
 while true do
@@ -14,5 +27,11 @@ while true do
     Ppu.draw_next_pixel ();
     if i mod 8 = 0 then Cpu.run_next_instruction ()
   done;
-  Unix.sleepf (30.0 /. 60.0);
+  
+  Unix.sleepf (1.0 /. 60.0);
+  
+  sdl_wrapper @@ Sdl.update_texture texture None Ppu.draw.bigarray (256 * 3);
+  sdl_wrapper @@ Sdl.render_clear renderer;
+  sdl_wrapper @@ Sdl.render_copy renderer texture;
+  Sdl.render_present renderer
 done
--- CAMLNES/lib/ppu.ml
+++ CAMLNES/lib/ppu.ml
@@ -44,25 +44,115 @@ let set_vblank_started boolean =
 type draw = {
   mutable x : int;
   mutable y : int;
-  screen : int array array
+  screen : int array array;
+  fg : int array array;
+  bigarray : (int, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array1.t;
 }
 
 let draw = {
   x = 0;
   y = 0;
-  screen = Array.make 240 (Array.make 256 0)
+  screen = Array.make 240 (Array.make 256 0);
+  fg = Array.make 240 (Array.make 256 (-1));
+  bigarray = Bigarray.Array1.create Bigarray.int8_unsigned Bigarray.c_layout (256 * 240 * 3)
 }
 
-let draw_background_pixel () = draw.screen.(draw.y).(draw.x) <- 1
+let colors = [| 0x747474; 0x24188c; 0x0000a8; 0x44009c; 0x8c0074; 0xa80010;
+0xa40000; 0x7c0800; 0x402c00; 0x004400; 0x005000; 0x003c14; 0x183c5c; 0x000000;
+0x000000; 0x000000; 0xbcbcbc; 0x0070ec; 0x2038ec; 0x8000f0; 0xbc00bc; 0xe40058;
+0xd82800; 0xc84c0c; 0x887000; 0x009400; 0x00a800; 0x009038; 0x008088; 0x000000;
+0x000000; 0x000000; 0xfcfcfc; 0x3cbcfc; 0x5c94fc; 0xcc88fc; 0xf478fc; 0xfc74b4;
+0xfc7460; 0xfc9838; 0xf0bc3c; 0x80d010; 0x4cdc48; 0x58f898; 0x00e8d8; 0x787878;
+0x000000; 0x000000; 0xfcfcfc; 0xa8e4fc; 0xc4d4fc; 0xd4c8fc; 0xfcc4fc; 0xfcc4d8;
+0xfcbcb0; 0xfcd8a8; 0xfce4a0; 0xe0fca0; 0xa8f0bc; 0xb0fccc; 0x9cfcf0; 0xc4c4c4;
+0x000000; 0x000000; |]
 
-let draw_foreground_pixel () = ()
+let get_CHR_tile table_addr number =
+  let start = table_addr + number * 16 in
+  let tile = Array.make 8 (Array.make 8 0) in
+  for i = 0 to 7 do
+    for j = 0 to 7 do
+      let bit1 = nth_bit (7 - j) (Ppumem.read_raw (start + i)) in
+      let bit2 = nth_bit (7 - j) (Ppumem.read_raw (start + 8 + i)) in
+      let res =
+        if not bit1 && not bit2 then 0
+        else if bit1 && not bit2 then 1
+        else if not bit1 && bit2 then 2
+        else 3
+      in tile.(i).(j) <- res
+    done
+  done;
+
+  tile
+
+let get_CHR_tile_colors sprite_palette palette_number table_addr number =
+  let palette_start = if sprite_palette then 0x3F10 else 0x3F00 in
+  let tile = get_CHR_tile table_addr number in
+  let tile_colors = Array.make 8 (Array.make 8 0x1D) in
+  for i = 0 to 7 do
+    for j = 0 to 7 do
+      if tile.(i).(j) = 0 && sprite_palette then tile_colors.(i).(j) <- -1
+      else if tile.(i).(j) = 0 then tile_colors.(i).(j) <- Ppumem.read_raw 0x3F00
+      else tile_colors.(i).(j) <-
+        Ppumem.read_raw (palette_start + palette_number * 4 + tile.(i).(j))
+    done
+  done;
+
+  tile_colors
+
+let render_sprites () =
+  for i = 0 to 239 do
+    for j = 0 to 255 do
+      draw.fg.(i).(j) <- -1
+    done
+  done;
+
+  for i = 0 to 63 do
+    let y_pos =
+      if Ppumem._OAM_read i = 255 then 255 else (Ppumem._OAM_read i) + 1 in
+    let tile_number = Ppumem._OAM_read (i + 1) in
+    let palette_number = Ppumem._OAM_read (i + 2) land 0b11 in
+    let x_pos = Ppumem._OAM_read (i + 3) in
+
+    let tile_colors = get_CHR_tile_colors
+      true palette_number (get_sprite_pattern_table_addr ()) tile_number in
+
+    for m = 0 to 7 do
+      for n = 0 to 7 do
+        try
+          draw.fg.(y_pos + m).(x_pos + n) <- tile_colors.(m).(n)
+        with _ -> ()
+      done
+    done
+
+  done
+
+let screen_to_bigarray () =
+  for y = 0 to 239 do
+    for x = 0 to 255 do
+      let rgb_color = colors.(draw.screen.(y).(x)) in
+      draw.bigarray.{(y * 256 + x) * 3} <- rgb_color lsr 16;
+      draw.bigarray.{(y * 256 + x) * 3 + 1} <- rgb_color lsr 8;
+      draw.bigarray.{(y * 256 + x) * 3 + 2} <- rgb_color
+    done
+  done
 
 let draw_next_pixel () =
-  if draw.x = 0 && draw.y = 0 then set_vblank_started false;
-  draw_background_pixel ();
+  if draw.x = 0 && draw.y = 0 then (
+    set_vblank_started false;
+
+    render_sprites ()
+  );
+
+  if draw.fg.(draw.y).(draw.x) <> -1 then draw.screen.(draw.y).(draw.x) <- draw.fg.(draw.y).(draw.x)
+  else draw.screen.(draw.y).(draw.x) <- 0x1D; (* 0x1D is black *)
+
   if draw.x = 255 && draw.y = 239 then (
+    screen_to_bigarray ();
+
     set_vblank_started true;
     if get_generate_NMI () then Cpu.state.nmi <- true
   );
+    
   draw.x <- (draw.x + 1) mod 256;
   if draw.x = 0 then draw.y <- (draw.y + 1) mod 240
