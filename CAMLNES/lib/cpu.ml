870f8d2adbb60836814179a219871eb2df23abf1
Adrian HEOUAIRI <adrian.heouairi@etu.u-paris.fr>
2023-04-22 21:20:13 +0200

    Make some raw reads not raw

--- CAMLNES/lib/cpu.ml
+++ CAMLNES/lib/cpu.ml
@@ -95,7 +95,7 @@ let stack_push byte =
 
 let stack_pull () =
   state.stack_pointer <- (state.stack_pointer + 1) mod 256;
-  Bus.read_raw (0x0100 lor state.stack_pointer)
+  Bus.read (0x0100 lor state.stack_pointer)
 
 (* Returns as a boolean bit n of an integer, LSB is 0 *)
 let get_nth_bit bit number = number land (1 lsl bit) > 0
@@ -170,7 +170,7 @@ let _BRK (calculated_addr, byte_at_addr) =
   stack_push (pc land 255);
   stack_push (status_to_byte () lor 0b0011_0000);
   state.interrupt_disable_flag <- true;
-  state.program_counter <- Bus.read_raw 0xFFFE + (Bus.read_raw 0xFFFF * 256)
+  state.program_counter <- Bus.read 0xFFFE + (Bus.read 0xFFFF * 256)
 
 let _BVC (calculated_addr, byte_at_addr) =
   if not state.overflow_flag then branch byte_at_addr
@@ -238,8 +238,6 @@ let _INY (calculated_addr, byte_at_addr) =
   state.negative_flag <- get_nth_bit 7 state.index_register_Y
 
 let _JMP (calculated_addr, byte_at_addr) =
-  (*Printf.printf "JMP address: %02X %02X " (Bus.read 0x02FF) (Bus.read 0x0200);
-    Printf.printf "JMP calculated address: %04X\n" calculated_addr;*)
   state.program_counter <- calculated_addr
 
 let _JSR (calculated_addr, byte_at_addr) =
@@ -514,28 +512,28 @@ let resolve_addr addr_mode following_byte_1 following_byte_2 =
   | Relative -> (-1, following_byte_1)
   | IndirectX ->
       let addr =
-        (Bus.read_raw ((following_byte_1 + state.index_register_X) mod 256)
-        + Bus.read_raw ((following_byte_1 + state.index_register_X + 1) mod 256)
+        (Bus.read ((following_byte_1 + state.index_register_X) mod 256)
+        + Bus.read ((following_byte_1 + state.index_register_X + 1) mod 256)
           * 256)
         mod 65536
       in
       (addr, Bus.read_raw addr)
   | IndirectY ->
       let addr =
-        (Bus.read_raw following_byte_1
-        + (Bus.read_raw ((following_byte_1 + 1) mod 256) * 256)
+        (Bus.read following_byte_1
+        + (Bus.read ((following_byte_1 + 1) mod 256) * 256)
         + state.index_register_Y)
         mod 65536
       in
       (addr, Bus.read_raw addr)
   | Indirect ->
       if following_byte_1 = 0xFF then
-        ( Bus.read_raw (following_byte_1 + (following_byte_2 * 256))
-          + (Bus.read_raw (following_byte_2 * 256) * 256),
+        ( Bus.read (following_byte_1 + (following_byte_2 * 256))
+          + (Bus.read (following_byte_2 * 256) * 256),
           -1 )
       else
-        ( Bus.read_raw (following_byte_1 + (following_byte_2 * 256))
-          + (Bus.read_raw (following_byte_1 + (following_byte_2 * 256) + 1) * 256),
+        ( Bus.read (following_byte_1 + (following_byte_2 * 256))
+          + (Bus.read (following_byte_1 + (following_byte_2 * 256) + 1) * 256),
           -1 )
 
 let run_next_instruction () =
@@ -547,9 +545,10 @@ let run_next_instruction () =
     stack_push (state.program_counter land 255);
     stack_push (status_to_byte ());
     state.interrupt_disable_flag <- true;
-    state.program_counter <- Bus.read_raw 0xFFFA + (Bus.read_raw 0xFFFB * 256));
+    state.program_counter <- Bus.read 0xFFFA + (Bus.read 0xFFFB * 256));
 
-  let opcode = Bus.read_raw state.program_counter in
+  let opcode = Bus.read state.program_counter in
+  (* TODO The two following reads should not be raw *)
   let following_byte_1 = Bus.read_raw ((state.program_counter + 1) mod 65536) in
   let following_byte_2 = Bus.read_raw ((state.program_counter + 2) mod 65536) in
 
